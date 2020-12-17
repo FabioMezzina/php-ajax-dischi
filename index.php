b9c782d0bf3c7142a8d7814923b8615536c7d934
Fabio Mezzina <fabio.mezzina.09@gmail.com>
2020-12-17 15:01:24 +0100

    repo init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-ajax-dischi</title>
+  <!-- Style -->
+  <link rel="stylesheet" href="./dist/css/main.css">
+</head>
+<body>
+  
+
+  <!-- JS -->
+  <script src="./dist/js/main.js"></script>
+</body>
+</html>
